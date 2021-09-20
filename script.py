a4757942e0eeb5c79437067e2e37bf7cba78a22b
jackkweyunga <jackkweyunga@gmail.com>
2021-09-20 23:32:28 +0300

    initial commit

---
+++ script.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import subprocess
+
+
+def record_ping(path):
+
+    df = pd.read_excel(path)
+
+    ips = df["ip"]
+
+    n = 0
+    print("pinging ... ")
+    new = pd.DataFrame({'ip': [i for i in ips], 'ping': [output(i) for i in ips]})
+    new.to_excel('new.xlsx')
+
+
+    
+
+def output(ip):
+    return subprocess.run(["ping",f"{ip}"], capture_output=True)
+
+
+record_ping("./ips.xlsx")
